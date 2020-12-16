2c473640a1b9c70e181fa8eb1e77b62daffcffd4
amitsharma21 <9amithp@gmail.com>
2020-12-16 15:55:45 +0530

    Burger App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
+          <h1>my name is amit sharma</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
