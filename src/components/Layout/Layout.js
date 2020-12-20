53d8d66e6452152fbb63e3451acea6bc703581a0
amitsharma21 <9amithp@gmail.com>
2020-12-20 16:27:36 +0530

    Burger Builder App

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Hoc from '../../Hoc/Hoc';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout=(props)=>(
-    <Hoc>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-         {props.children}
-        </main>
-    </Hoc>
-);
-export default layout;
+class Layout extends Component{
+    state={
+        showSideDrawer:false
+    }
+    sideDrawerClosedHandler=()=>{
+        this.setState({showSideDrawer:false})
+    }
+    sideDrawerToggleHandler=()=>{
+        this.setState((preState)=>{
+            return {showSideDrawer:!preState.showSideDrawer};
+        });
+    }
+    render(){
+        return(
+            <Hoc>
+             <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+             <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler}/>
+             <main className={classes.Content}>
+             {this.props.children}
+             </main>
+            </Hoc>
+        );
+    }
+}
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,25 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './SideDrawer.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Hoc from '../../../Hoc/Hoc';
 const sideDrawer=(props)=>{
+    let attachedClasses=[classes.SideDrawer,classes.Close];
+    if(props.open){
+        attachedClasses=[classes.SideDrawer,classes.Open];
+    }
     return(
-        <div className={classes.SideDrawer}>
+        <Hoc>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
             <div className={classes.Logo}>
              <Logo/>
             </div>
             <nav>
                 <NavigationItems/>
             </nav>
-        </div>
+            </div>
+        </Hoc>
     );
 };
 
